088d804d546095c1b5adbe1643b9ed171d7893f2
abishek3358 <abishek582000@gmail.com>
2025-10-24 10:23:13 -0500

    Auth

--- src/main/java/com/app/util/IConstant.java
+++ src/main/java/com/app/util/IConstant.java
@@ -1,11 +1,11 @@
 package com.app.util;
 
 public class IConstant {
-    public static final String BASE_PACKAGE = "com.app";
-    public static final String API_DETAILS = "Ebrain Local Lending API Documentation";
-    public static final double MIN = 100;
+	public static final String BASE_PACKAGE = "com.app";
+	public static final String API_DETAILS = "Care Connect API Documentation";
+	public static final double MIN = 100;
 	public static final double TDF_MIN = -50;
 	public static final double ENDURANCE_MIN = 100;
-	public static final long OTP_VALID_DURATION = 10*1000*60;
+	public static final long OTP_VALID_DURATION = 10 * 1000 * 60;
 	public static final String ROLE_USER = "user_role";
 }
